18a805319de336d678dbd4e5a00bbeb19054a8ac
Tasha <shradertasha@gmail.com>
2019-03-07 16:06:40 -0600

    next

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,48 +1,47 @@
-$(function(){
+$(function () {
 
-  $(".change-devoured").on("click", function(event){
-      var id = $(this).data("id");
-      var yesDevoured= $(this).data("newBurger");
+  $(".change-devoured").on("click", function (event) {
+    var id = $(this).data("id");
+    var yesDevoured = $(this).data("newBurger");
 
-      var newState = {
-          devoured: yesDevoured
-      };
-      $.ajax("/api/burgers" + id, {
-          type: "PUT",
-          data: newState
-      }).then(
-          function(){
-              location.reload();
-          }
-      );
+    var newState = {
+      devoured: yesDevoured
+    };
+    $.ajax("/api/burgers" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function (dbResponse) {
+        res.json(dbResponse);
+      });
   });
 
 
 });
 
-$(".create-form").on("submit-burger", function(event, req, res){
-event.preventDefault();
-var newBurger ={
-  name: req.body.burger_name,
-  devoured: req.body.devoured
-}
-$.ajax("/api/burger/", {
-  name: "Post",
-  data: newBurger,
-}).then(function(){
-  location.reload();
-})
+$(".create-form").on("submit-burger", function (event, req, res) {
+  event.preventDefault();
+  var newBurger = {
+    name: req.body.burger_name,
+    devoured: req.body.devoured
+  }
+  $.ajax("/api/burger/", {
+    name: "Post",
+    data: newBurger,
+  }).then(function (dbResponse) {
+    res.json(dbResponse);
+  })
 
 })
 
-$(".delete-burger").on("click", function(event){
+$(".delete-burger").on("click", function (event) {
   var id = $(this).data("id");
   $.ajax("/api/burger/" + id, {
-      type: "DELETE"
+    type: "DELETE"
   }).then(
-      function() {
-          location.reload();
-      }
+    function (dbResponse) {
+      res.json(dbResponse);
+    }
   )
 })
 
